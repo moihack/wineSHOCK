7ddcde5dd9a7051ef4318429e409f54a130ca9bf
moihack <moihack.inside@gmail.com>
2018-05-20 18:01:47 +0300

    Upload custom Unigine Heaven test-profile.
    This downloads and runs the latest version of Unigine Heaven benchmark.
    It runs the DirectX 9 version of the benchmark.
    On Linux it installs and runs the benchmark via Wine.

---
+++ heaven_wine/install_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "#!/bin/sh
+cd \"C:\Program Files (x86)\Unigine\Heaven Benchmark 4.0\bin\"
+./Heaven.exe -video_app direct3d9 -data_path ../ -sound_app null -engine_config ../data/heaven_4.0.cfg -system_script heaven/unigine.cpp -video_mode -1 -extern_define PHORONIX,RELEASE \$@ > \$LOG_FILE" > heaven_wine
+
+# This assumes you will install to the default location
+# C:\Program Files (x86)\Unigine\
+./Unigine_Heaven-4.0.exe
