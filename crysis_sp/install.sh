ac393dcc9b603d94c2765621d3c504c82d08f77f
moihack <moihack.inside@gmail.com>
2018-08-03 11:13:35 +0300

    Update Crysis SP Demo to keep track with recent changes on openbenchmarking

--- crysis_sp/install.sh
+++ crysis_sp/install.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
 chmod +x nzd_crysis_spdemo.exe
-./nzd_crysis_spdemo.exe -s2 -sp"/s /v/qn"
+wine ./nzd_crysis_spdemo.exe -s2 -sp"/s /v/qn"
 
 echo '#!/bin/sh
 rm "$WINEPREFIX/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Game/Levels/island/benchmark_gpu.log" 2> /dev/null
 cd "$WINEPREFIX/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Bin64"
 
-./crysis.exe -dx9 -DEVMODE +map island +exec benchmark_gpu
-mygrep=$(grep -o "Average FPS: [0-9][0-9].[0-9][0-9]" "$WINEPREFIX/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Game/Levels/island/benchmark_gpu.log" | grep -o [0-9][0-9].[0-9][0-9])
+wine ./crysis.exe -dx9 -DEVMODE +map island +exec benchmark_gpu
 
-fps=0
-while read -r line
-do
-	fps=$(echo "$fps+$line" | bc -l)
-done <<< "$mygrep"
-fps=$(echo "$fps/4" | bc -l | cut -c1-5)
+fps=$(grep -o "Average FPS: [0-9][0-9].[0-9][0-9]" "$WINEPREFIX/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Game/Levels/island/benchmark_gpu.log" | grep -o [0-9][0-9].[0-9][0-9] | awk '\''{ total += $1; count++ } END { print total/count }'\'')
+
 echo -e $"Crysis Benchmark Results\nAverage FPS: " $fps > $LOG_FILE
 ' > crysis_sp
 
--- crysis_sp/install_windows.sh
+++ crysis_sp/install_windows.sh
@@ -27,14 +27,9 @@ rm "C:/pts-benchmarks/crysis/Game/Levels/island/benchmark_gpu.log" 2> /dev/null
 cd "C:/pts-benchmarks/crysis/Bin64"
 
 ./crysis.exe -dx9 -DEVMODE +map island +exec benchmark_gpu
-mygrep=$(grep -o "Average FPS: [0-9][0-9].[0-9][0-9]" "C:/pts-benchmarks/crysis/Game/Levels/island/benchmark_gpu.log" | grep -o [0-9][0-9].[0-9][0-9])
 
-fps=0
-while read -r line
-do
-	fps=$(echo "$fps+$line" | bc -l)
-done <<< "$mygrep"
-fps=$(echo "$fps/4" | bc -l | cut -c1-5)
+fps=$(grep -o "Average FPS: [0-9][0-9].[0-9][0-9]" "C:/pts-benchmarks/crysis/Game/Levels/island/benchmark_gpu.log" | grep -o [0-9][0-9].[0-9][0-9] | awk '\''{ total += $1; count++ } END { print total/count }'\'')
+
 echo -e $"Crysis Benchmark Results\nAverage FPS: " $fps > $LOG_FILE
 ' > crysis_sp
 
