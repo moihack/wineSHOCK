7b0c2c2ea32141cdd2131b4b653728823d26867e
moihack <moihack@users.noreply.github.com>
2018-08-07 02:25:02 +0300

    Update pts_prerun.sh

--- pts_prerun.sh
+++ pts_prerun.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-export WINEPREFIX=~/ptsprefix
-export WINEPREFIX=~/.wine
+#you must source this file before running PTS from a new terminal window
+
+#(un)comment any variable you may need below
+#you must keep WINEPREFIX at all times,
+#unless you set it somewhere else (e.g. bash.rc)
+
+export WINEPREFIX=~/.wine #CAUTION! Needed for all tests! 
 export WINEDEBUG=-all
-export WINE_VERSION=`wine --version`
+export WINE_VERSION=`wine --version` #Needed for PTS to report wine version to system-layer
 export TEST_RESULTS_NAME="$WINE_VERSION"
 export TEST_RESULTS_IDENTIFIER="`hostname -s`-st: $WINE_VERSION"
-export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEDLLOVERRIDES="mscoree,mshtml=" #Disable wine pop-ups for Mono and Gecko not being installed
+#export TMUSER="" #your TrackMania Nations Forever username - for tmnations test-profile
+#export TMPWD=""  #your TrackMania Nations Forever password - for tmnations test-profile
+#export STEAMUSER="" #your Steam username - for Tomb Raider (2013) test-profile
+#export STEAMPWD=""  #your Steam password - for Tomb Raider (2013) test-profile
